e1e58a32fd4a02abc3cf648b1213eb8f1bd8db1e
Tim Johnson <jhnsntmthy@gmail.com>
2020-10-07 12:47:29 -0400

    heckin grid action

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Heading } from 'theme-ui'
+import { Card, Container, Grid, Heading, Text } from 'theme-ui'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -7,7 +7,21 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Container>
-      <Heading as="h1">Very Fine Bois</Heading>
+      <Heading as="h1">Chokers</Heading>
+      <Grid columns={[1, 2, 3]} gap={20}>
+        <Card>
+          <Heading>A very fine boi</Heading>
+          <Text>Lorem ITextsum</Text>
+        </Card>
+        <Card>
+          <Heading>One Heckin Ch0nker</Heading>
+          <Text>Lorem ITextsum</Text>
+        </Card>
+        <Card>
+          <Heading>Mega Ch0nker</Heading>
+          <Text>Lorem ITextsum</Text>
+        </Card>
+      </Grid>
     </Container>
   </Layout>
 )
