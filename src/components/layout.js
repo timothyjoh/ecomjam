b6de3d650cf8c318b20898e65584c43d34b44f48
Tim Johnson <jhnsntmthy@gmail.com>
2020-10-07 12:33:07 -0400

    first round

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,11 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import { graphql, useStaticQuery } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -30,16 +29,18 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: `0 1.0875rem 1.45rem`
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <footer
+          style={{
+            marginTop: `2rem`
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.com">Mah Turds</a>
         </footer>
       </div>
     </>
@@ -47,7 +48,7 @@ const Layout = ({ children }) => {
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    text: '#fff',
+    background: '#000',
+    primary: '#345'
+  },
+  fonts: {
+    body: 'Public Sans',
+    heading: 'Public Sans',
+    footer: 'Comic Neue'
+  },
+  fontWeights: {
+    body: 400,
+    heading: 100,
+    bold: 900
+  }
+}
